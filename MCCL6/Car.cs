6781676e4fd9e15858bc5b1264d8c2295420f450
ртивмыли <BaklanovNM@gmail.com>
2023-03-23 17:14:36 +0200

    Complete class "Car" and "IID"

---
+++ MCCL6/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCCL6
+{
+    internal class Car : IID
+    {
+        public int Id { get; set; }
+        public string CarBrand { get; set; }
+        public string CarModel { get; set; }
+
+        public Car(string carBrand, string carModel)
+        {
+            CarBrand = carBrand;
+            CarModel = carModel;
+        }
+        public override string ToString()
+        {
+            return string.Format(Id + " " + CarBrand + " " + CarModel);
+        }
+    }
+}
