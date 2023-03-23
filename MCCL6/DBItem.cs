e963675ca0108ef04fa50f127ce9ba8c9161bd44
kinde <kinde@DESKTOP-O2QTT0D>
2023-03-23 17:23:11 +0200

    added DBItem and IDBItem

---
+++ MCCL6/DBItem.cs
@@ -0,0 +1,26 @@
+﻿using MCCL6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9MCCL6
+{
+    internal class DBItem<T> : IDBItem<T> where T : IID
+    {
+        private int counter = 1;
+        public List<T> Items { get; set; }
+
+        public DBItem()
+        {
+            Items = new List<T>();
+        }
+
+        public void AddItem(T item)
+        {
+            item.Id = counter++;
+            Items.Add(item);
+        }
+    }
+}
---
+++ MCCL6/IDBItem.cs
@@ -0,0 +1,11 @@
+﻿using MCCL6;
+
+namespace _9MCCL6
+{
+    internal interface IDBItem<T> where T : IID
+    {
+        List<T> Items { get; set; }
+
+        void AddItem(T item);
+    }
+}
