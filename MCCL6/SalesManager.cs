53ccf2d7e374f18be69686d032eab19e267d1000
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-03-23 17:35:00 +0200

    added "SalesManager" and "Menu"

---
+++ MCCL6/SalesManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9MCCL6
+{
+    internal class SalesManager
+    {
+        IDBItem<Car> dbCar;
+
+        public SalesManager(IDBItem<Car> dbCar)
+        {
+            this.dbCar = dbCar;
+        }
+
+        public void CreateCar()
+        {
+            Console.Write("Car Brand?: ");
+            string carBrand = Console.ReadLine();
+            Console.Write("Car Model?: ");
+            string carModel = Console.ReadLine();
+            Car car = new Car(carBrand,carModel);
+            dbCar.AddItem(car);
+        }
+    }
+}
